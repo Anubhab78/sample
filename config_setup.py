f1bf10d8a36f5278dca6dd55d62411621594edc5
Anubhab <65456443+Anubhab78@users.noreply.github.com>
2022-03-10 19:39:19 +0530

    Create config_setup.py

---
+++ config_setup.py
@@ -0,0 +1,79 @@
+# from schema import Schema, And, Use, Optional, SchemaError
+# import yaml
+#
+# schema = Schema(
+#         {
+#             'SAST':
+#                 'Sonarqube':
+#                    'repo'
+#                    'SONAR_HOST_URL':,
+#                    'SONAR_PROJECT_KEY':,
+#
+#             'DAST':,
+#                 'zap':,
+#                     'repo': ,
+#                     'typeofscan': ,
+#                     'target': ,
+#                     'format': ,
+#                     'options': ,
+#
+#             'Tekton-build-and-push':,
+#                 'docker'
+#                     'repo': ,
+#                     'docker_username': ,
+#                     'docker_password': ,
+#                     'image_name':,
+#                     'revision': ,
+#                     'url': ,
+#
+#         }
+#     )
+#
+# with open('Config.yaml') as f:
+#    data = yaml.load(f)
+#    try:
+#        schema.validate(data)
+#    except SchemaError as e:
+#        print(e)
+
+#
+#
+# import yaml
+# from yaml.loader import SafeLoader
+# import cerberus
+#
+# def load_doc():
+#     data = ''
+#     with open('./Config.yaml') as f:
+#         data = yaml.load(f, Loader=SafeLoader)
+#         print(data)
+#     return data
+#
+#
+# ## Now, validating the yaml file:
+#
+# schema = eval(open('./schema.py').read())
+# v = cerberus.Validator(schema)
+# doc = load_doc()
+# print(v.validate(doc, schema))
+
+
+
+
+import yaml
+import cerberus
+
+def load_doc():
+    with open('./Config.yaml', 'r') as stream:
+        try:
+            return yaml.load(stream)
+        except yaml.YAMLError as exception:
+            raise exception
+
+## Now, validating the yaml file is straightforward:
+
+schema = eval(open('./schema.py', 'r').read())
+v = cerberus.Validator(schema)
+doc = load_doc()
+print(v.validate(doc, schema))
+print(v.errors)
